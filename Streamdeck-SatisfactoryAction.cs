dc4889328c64b3db53a64379a6e0bcf4c4e5871f
Brett Parker <brettleon123@gmail.com>
2022-03-14 18:48:12 +0000

    Stable

--- Streamdeck-SatisfactoryAction.cs
+++ Streamdeck-SatisfactoryAction.cs
@@ -7,30 +7,12 @@ using System.Threading.Tasks;
 
 namespace streamdecksatisfactory
 {
-  [ActionUuid(Uuid="envy.satisfactory.plugin.DefaultPluginAction")]
+  [ActionUuid(Uuid= "envy.satisfactory.buildings.miner")]
   public class StreamdeckSatisfactoryAction : BaseStreamDeckActionWithSettingsModel<Models.CounterSettingsModel>
   {
 	public override async Task OnKeyUp(StreamDeckEventPayload args)
 	{
-	  SettingsModel.Counter++;
-	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
-
-	  if (SettingsModel.Counter % 10 == 0)
-	  {
-		await Manager.ShowAlertAsync(args.context);
-	  }
-	  else if (SettingsModel.Counter % 15 == 0)
-	  {
-		await Manager.OpenUrlAsync(args.context, "https://www.bing.com");
-	  }
-	  else if (SettingsModel.Counter % 3 == 0)
-	  {
-		await Manager.ShowOkAsync(args.context);
-	  }
-	  else if (SettingsModel.Counter % 7 == 0)
-	  {
-		await Manager.SetImageAsync(args.context, "images/Fritz.png");
-	  }
+	  await Manager.SetTitleAsync(args.context, "Test");
 
 	  //update settings
 	  await Manager.SetSettingsAsync(args.context, SettingsModel);
@@ -39,13 +21,13 @@ namespace streamdecksatisfactory
 	public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
 	{
 	  await base.OnDidReceiveSettings(args);
-	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
+	  await Manager.SetTitleAsync(args.context, "Test");
 	}
 
 	public override async Task OnWillAppear(StreamDeckEventPayload args)
 	{
 	  await base.OnWillAppear(args);
-	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
+	  await Manager.SetTitleAsync(args.context, "Test");
 	}
 
   }
--- models/CounterSettingsModel.cs
+++ models/CounterSettingsModel.cs
@@ -2,6 +2,5 @@
 {
   public class CounterSettingsModel
   {
-	public int Counter { get; set; } = 0;
   }
 }
