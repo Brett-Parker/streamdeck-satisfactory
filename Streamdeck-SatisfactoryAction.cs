a83590b21893cc7014638701ae78d6dc8b8859de
Brett Parker <brettleon123@gmail.com>
2022-03-14 20:22:00 +0000

    Working POC
    Working example of a creation of a miner!

--- Streamdeck-SatisfactoryAction.cs
+++ Streamdeck-SatisfactoryAction.cs
@@ -2,33 +2,48 @@
 using StreamDeckLib.Messages;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WindowsInput.Native;
+using WindowsInput;
 
 namespace streamdecksatisfactory
 {
   [ActionUuid(Uuid= "envy.satisfactory.buildings.miner")]
   public class StreamdeckSatisfactoryAction : BaseStreamDeckActionWithSettingsModel<Models.CounterSettingsModel>
   {
+
+		InputSimulator sim = new InputSimulator();
+
 	public override async Task OnKeyUp(StreamDeckEventPayload args)
 	{
-	  await Manager.SetTitleAsync(args.context, "Test");
+		await Manager.SetTitleAsync(args.context, "Miner");
 
-	  //update settings
-	  await Manager.SetSettingsAsync(args.context, SettingsModel);
+		sim.Keyboard.KeyPress(VirtualKeyCode.VK_N);
+		sim.Keyboard.Sleep(50);
+		sim.Keyboard.TextEntry("Miner");
+		sim.Keyboard.Sleep(50);
+		sim.Keyboard.KeyDown(VirtualKeyCode.RETURN);
+		sim.Keyboard.Sleep(50);
+		sim.Keyboard.KeyUp(VirtualKeyCode.RETURN);
+
+		//update settings
+		await Manager.SetSettingsAsync(args.context, SettingsModel);
 	}
 
 	public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
 	{
-	  await base.OnDidReceiveSettings(args);
-	  await Manager.SetTitleAsync(args.context, "Test");
+		await base.OnDidReceiveSettings(args);
+		await Manager.SetTitleAsync(args.context, "Miner");
 	}
 
 	public override async Task OnWillAppear(StreamDeckEventPayload args)
 	{
-	  await base.OnWillAppear(args);
-	  await Manager.SetTitleAsync(args.context, "Test");
+		await base.OnWillAppear(args);
+		await Manager.SetTitleAsync(args.context, "Miner");
 	}
-
   }
 }
