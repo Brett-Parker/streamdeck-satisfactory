e67be25ea97df906882fc274b4d95f059a6d856f
Brett Parker <brettleon123@gmail.com>
2022-03-15 19:39:42 +0000

    Method definition

--- Streamdeck-SatisfactoryAction.cs
+++ Streamdeck-SatisfactoryAction.cs
@@ -3,29 +3,63 @@ using StreamDeckLib.Messages;
 using System.Threading.Tasks;
 using WindowsInput.Native;
 using WindowsInput;
+using System;
 
 namespace streamdecksatisfactory
 {
-  [ActionUuid(Uuid= "envy.satisfactory.buildings.miner")]
+  [ActionUuid(Uuid= "envy.satisfactory.buildings")]
   public class StreamdeckSatisfactoryAction : BaseStreamDeckActionWithSettingsModel<Models.CounterSettingsModel>
   {
-
+	// Variable decleration
 	InputSimulator sim = new InputSimulator();
+    private VirtualKeyCode virtualKeyCode;
 
-	public override async Task OnKeyUp(StreamDeckEventPayload args)
+	// StreamDeck button press
+    public override async Task OnKeyUp(StreamDeckEventPayload args)
 	{
 		await Manager.SetTitleAsync(args.context, "Miner");
+		if (Enum.TryParse<VirtualKeyCode>("VK_N", out virtualKeyCode))
+		{
+			KeyPress("VK_N");
+			KeyboardSleep(50);
+			sim.Keyboard.TextEntry("Miner");
+			KeyboardSleep(50);
+		}
+        else 
+		{
+			return;
+		}
 
-		sim.Keyboard.KeyPress(VirtualKeyCode.VK_N);
-		sim.Keyboard.Sleep(50);
-		sim.Keyboard.TextEntry("Miner");
-		sim.Keyboard.Sleep(50);
-		sim.Keyboard.KeyDown(VirtualKeyCode.RETURN);
-		sim.Keyboard.Sleep(50);
-		sim.Keyboard.KeyUp(VirtualKeyCode.RETURN);
+		PressReturn();
 
 		//update settings
 		await Manager.SetSettingsAsync(args.context, SettingsModel);
+	}
+
+	/// <summary>
+	/// KeyPress simulations a keypress input
+	/// </summary>
+    private void KeyPress(string input)
+	{
+		sim.Keyboard.KeyPress(virtualKeyCode);
+	}
+
+	/// <summary>
+	/// PressReturn simulations a enter/return input
+	/// </summary>
+    private void KeyboardSleep(int time)
+    {
+		sim.Keyboard.Sleep(time);
+	}
+
+	/// <summary>
+	/// PressReturn simulations a enter/return input
+	/// </summary>
+    private void PressReturn()
+    {
+		sim.Keyboard.KeyDown(VirtualKeyCode.RETURN);
+		sim.Keyboard.Sleep(50);
+		sim.Keyboard.KeyUp(VirtualKeyCode.RETURN);
 	}
 
 	public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
