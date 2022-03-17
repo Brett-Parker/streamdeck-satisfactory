b860748c7c904dde2f5ffc6048b8f1f360360173
Brett Parker <brettleon123@gmail.com>
2022-03-17 21:28:07 +0000

    Ship it!

--- src/Streamdeck-SatisfactoryAction.cs
+++ src/Streamdeck-SatisfactoryAction.cs
@@ -3,31 +3,47 @@ using StreamDeckLib.Messages;
 using System.Threading.Tasks;
 using WindowsInput.Native;
 using WindowsInput;
+using System.IO;
+using System;
 
 namespace streamdecksatisfactory
 {
   [ActionUuid(Uuid= "envy.satisfactory.buildings")]
   public class StreamdeckSatisfactoryAction : BaseStreamDeckActionWithSettingsModel<Models.SatisfactorySettingsModel>
   {
-    // Variable decleration
+    // Keyboard simulator
     readonly InputSimulator sim = new();
+	string path = Path.Combine(Environment.CurrentDirectory, @"images\");
 
 	// StreamDeck button press
     public override async Task OnKeyUp(StreamDeckEventPayload args)
 	{
-		await Manager.SetTitleAsync(args.context, "");
-
 		// Open search bar
 		sim.Keyboard.KeyPress(VirtualKeyCode.VK_N);
 		KeyboardSleep(50);
 
 		// Text entry to search. E.G "Miner"
-		sim.Keyboard.TextEntry(SettingsModel.SearchText);
+		string searchText = SettingsModel.SearchText[0..^4].Replace('_', ' ');
+		sim.Keyboard.TextEntry(searchText);
 		KeyboardSleep(50);
 		PressReturn();
 
-		//update settings
+		// Update settings
 		await Manager.SetSettingsAsync(args.context, SettingsModel);
+	}
+
+	public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
+	{
+		await base.OnDidReceiveSettings(args);
+
+        // Set icon
+		path = Path.Combine(path, SettingsModel.SearchText);
+		await Manager.SetImageAsync(args.context, path);
+	}
+
+	public override async Task OnWillAppear(StreamDeckEventPayload args)
+	{
+		await base.OnWillAppear(args);
 	}
 
 	/// <summary>
@@ -46,18 +62,6 @@ namespace streamdecksatisfactory
 		sim.Keyboard.KeyDown(VirtualKeyCode.RETURN);
 		sim.Keyboard.Sleep(50);
 		sim.Keyboard.KeyUp(VirtualKeyCode.RETURN);
-	}
-
-	public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
-	{
-		await base.OnDidReceiveSettings(args);
-		await Manager.SetTitleAsync(args.context, "");
-	}
-
-	public override async Task OnWillAppear(StreamDeckEventPayload args)
-	{
-		await base.OnWillAppear(args);
-		await Manager.SetTitleAsync(args.context, "");
 	}
   }
 }
