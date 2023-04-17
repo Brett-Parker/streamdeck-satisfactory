8bd14e179bea46916a7464097ab76fc8e791d84c
Brett Parker <brettleon123@gmail.com>
2023-04-17 14:56:41 +0100

    Program tidy

--- src/Program.cs
+++ src/Program.cs
@@ -5,20 +5,14 @@ namespace streamdecksatisfactory
 {
     class Program
     {
-
         static async Task Main(string[] args)
         {
-
             using (var config = StreamDeckLib.Config.ConfigurationBuilder.BuildDefaultConfiguration(args))
             {
-
                 await ConnectionManager.Initialize(args, config.LoggerFactory)
                                                              .RegisterAllActions(typeof(Program).Assembly)
                                                              .StartAsync();
-
             }
-
         }
-
     }
 }
